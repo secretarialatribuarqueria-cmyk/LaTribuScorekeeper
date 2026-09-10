bee2e7df403b5a371881e9a9272a82f508998e5b
secretarialatribuarqueria-cmyk <secretarialatribuarqueria@gmail.com>
2026-09-10 20:56:02 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Permite que Vercel termine el build exitosamente
     ignoreBuildErrors: true,
   },
   eslint: {
